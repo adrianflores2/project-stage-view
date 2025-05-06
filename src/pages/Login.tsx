c89bf41b870c1e0001eb7565b056f3bb90bfc8dc
adrianflores2 <adrodfp220@gmail.com>
2025-05-06 00:09:46 -0500

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-
+import { supabase } from "@/integrations/supabase/client";
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAppContext } from '@/context/AppContext';
